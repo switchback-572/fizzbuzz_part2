a3e219d1e0a127e660ff9e895cf2a91457b5494e
switchback <switchback.572@gmail.com>
2019-10-19 23:47:48 +0900

    リファクタリング:が−ド節を採用してみた｡

--- fizuubuzz.py
+++ fizuubuzz.py
@@ -1,12 +1,11 @@
 def fizzbuzz_convert(number):
     if number % 15 == 0:
         return "FizzBuzz"
-    elif number % 3 == 0:
+    if number % 3 == 0:
         return "Fizz"
-    elif number % 5 == 0:
+    if number % 5 == 0:
         return "Buzz"
-    else:
-        return str(number)
+    return str(number)
 
 
 result = fizzbuzz_convert(1)
